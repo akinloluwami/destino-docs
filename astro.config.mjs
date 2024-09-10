d0236b3b7ae3fce2b9d1aaa46cfe2cdcfa80af8e
akinloluwami <akinkunmioye42@gmail.com>
2024-09-10 07:34:46 +0100

    Docs setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,45 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Destino docs",
+      customCss: ["./src/custom.css"],
+      sidebar: [
+        {
+          label: "Intro",
+          slug: "docs",
+          collapsed: false,
+        },
+        {
+          label: "Getting Started",
+          collapsed: false,
+          items: [
+            "docs/start/installation",
+            "docs/start/manual-setup",
+            "docs/start/project-structure-overview",
+          ],
+        },
+        {
+          label: "File-based Routing",
+          collapsed: false,
+          items: ["docs/routing/static", "docs/routing/dynamic"],
+        },
+        {
+          label: "Middlewares",
+          collapsed: false,
+          items: [
+            "docs/middlewares/defining-middlewares",
+            "docs/middlewares/middleware-file",
+          ],
+        },
+        {
+          label: "Configuration",
+          collapsed: false,
+          items: ["docs/configuration/default", "docs/configuration/custom"],
+        },
+      ],
+    }),
+  ],
+});
